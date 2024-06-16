0b8a6a0fc7ffacd10721553976bc21a7bcb6b633
ZikaZaki <17160061@su.edu.ye>
2024-06-16 20:35:01 +0300

    update Listings hooks

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -5,12 +5,13 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   Image,
+  FlatList,
+  StyleSheet,
   NativeSyntheticEvent,
   NativeScrollEvent,
 } from "react-native";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { defaultStyles } from "@/constants/Styles";
-import { FlatList, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import { AirbnbList } from "@/app/interfaces/airbnb_list";
 import { Ionicons } from "@expo/vector-icons";
@@ -24,6 +25,7 @@ interface ListingsProps {
 
 const Listings: React.FC<ListingsProps> = ({ items, category }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [imageLoading, setImageLoading] = useState(true);
   const [data, setData] = useState<AirbnbList[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const listRef = useRef<FlatList<AirbnbList>>(null);
@@ -31,99 +33,135 @@ const Listings: React.FC<ListingsProps> = ({ items, category }) => {
 
   useEffect(() => {
     loadInitialItems();
-    console.log("items: ", data.length);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 200);
   }, [category]);
 
   const loadInitialItems = useCallback(() => {
     setIsLoading(true);
-    setData(items.slice(0, 20));
+    const initialData = items.slice(0, 20);
+    setData(initialData);
     setIsLoading(false);
   }, [items]);
 
   const loadMoreData = useCallback(() => {
     if (isLoading) return;
     setIsLoading(true);
-    setCurrentPage(currentPage + 1);
-    const startIndex = (currentPage - 1) * 20;
+    const startIndex = currentPage * 20;
     const endIndex = startIndex + 20;
-    setData((prevData) => [...prevData, ...items.slice(startIndex, endIndex)]);
+    const moreData = items.slice(startIndex, endIndex);
+    setData((prevData) => [...prevData, ...moreData]);
+    setCurrentPage((prevPage) => prevPage + 1);
+    setIsLoading(false);
   }, [isLoading, currentPage, items]);
 
-  const renderRow: ListRenderItem<AirbnbList> = ({ item }) => (
-    <Animated.View
-      style={styles.listing}
-      entering={FadeInRight}
-      exiting={FadeOutLeft}
-    >
-      <Link href={`/listing/${item.id}`} key={item.id + item.host_id} asChild>
-        <TouchableOpacity>
-          <Image source={{ uri: item.picture_url }} style={styles.image} />
-          <TouchableOpacity style={styles.likeBtn}>
-            <Ionicons name="heart-outline" size={24} color="#000" />
+  const RenderRow: ListRenderItem<AirbnbList> = ({ item }) => {
+    return (
+      <Animated.View
+        style={styles.listing}
+        entering={FadeInRight}
+        exiting={FadeOutLeft}
+      >
+        <Link href={`/listing/${item.id}`} key={item.id + item.host_id} asChild>
+          <TouchableOpacity>
+            <View style={styles.imageContainer}>
+              {imageLoading && (
+                <View
+                  style={{
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: 10,
+                  }}
+                >
+                  <ActivityIndicator size="large" color="#FF5A5F" />
+                  <Text
+                    style={{
+                      fontFamily: "mon-sb",
+                      fontSize: 14,
+                      color: "grey",
+                    }}
+                  >
+                    Loading image...
+                  </Text>
+                </View>
+              )}
+              <Image
+                style={styles.image}
+                source={{
+                  uri: item.picture_url,
+                }}
+                onLoad={() =>
+                  setTimeout(() => {
+                    setImageLoading(false);
+                  }, 2000)
+                }
+                onError={() => setImageLoading(false)}
+              />
+            </View>
+
+            <TouchableOpacity style={styles.likeBtn}>
+              <Ionicons name="heart-outline" size={24} color="#FF5A5F" />
+            </TouchableOpacity>
+            <View style={styles.imageAfter} />
           </TouchableOpacity>
-          <View style={styles.imageAfter} />
-        </TouchableOpacity>
-      </Link>
+        </Link>
 
-      <View style={styles.priceContainer}>
-        <View style={styles.priceBefore} />
-        <View style={styles.price}>
-          <Text style={{ fontFamily: "mon-b", color: "white" }}>
-            Price: {item.price ? item.price + "night" : "  N/A"}
-          </Text>
-        </View>
-        <View style={styles.priceAfter}>
-          <View
-            style={{
-              height: 12.5,
-              width: 12.5,
-              backgroundColor: "#FF5A5F",
-              borderRadius: 6.25,
-            }}
-          />
-        </View>
-      </View>
-
-      <View style={styles.details}>
-        <Text
-          style={{
-            fontSize: 14,
-            fontFamily: "mon-sb",
-            width: "100%",
-          }}
-          numberOfLines={1}
-          ellipsizeMode="tail"
-        >
-          {item.name}
-        </Text>
-
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            gap: 4,
-          }}
-        >
-          <Text style={{ fontFamily: "mon-sb", color: Colors.grey }}>
-            Accommodates: {item.accommodates}
-          </Text>
-
-          <View style={{ flexDirection: "row", gap: 2 }}>
-            <Text style={{ fontFamily: "mon-sb", color: Colors.grey }}>
-              Rating:{" "}
-            </Text>
-            <Ionicons name="star" size={16} />
-            <Text style={{ fontFamily: "mon-sb" }}>
-              {item.review_scores_value}
+        <View style={styles.priceContainer}>
+          <View style={styles.priceBefore} />
+          <View style={styles.price}>
+            <Text style={{ fontFamily: "mon-b", color: "white" }}>
+              Price: {item.price ? item.price + "night" : "  N/A"}
             </Text>
           </View>
+          <View style={styles.priceAfter}>
+            <View
+              style={{
+                height: 15,
+                width: 15,
+                backgroundColor: "#FF5A5F",
+                borderRadius: 50,
+              }}
+            />
+          </View>
         </View>
-      </View>
-    </Animated.View>
-  );
+
+        <View style={styles.details}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontFamily: "mon-sb",
+              width: "100%",
+            }}
+            numberOfLines={1}
+            ellipsizeMode="tail"
+          >
+            {item.name}
+          </Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              gap: 4,
+            }}
+          >
+            <Text style={{ fontFamily: "mon-sb", color: Colors.grey }}>
+              Accommodates: {item.accommodates}
+            </Text>
+
+            <View style={{ flexDirection: "row", gap: 2 }}>
+              <Text style={{ fontFamily: "mon-sb", color: Colors.grey }}>
+                Rating:{" "}
+              </Text>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_value}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </Animated.View>
+    );
+  };
 
   const renderFooter = () => {
     if (isLoading) {
@@ -144,9 +182,6 @@ const Listings: React.FC<ListingsProps> = ({ items, category }) => {
 
   const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     const currentOffset = event.nativeEvent.contentOffset.y;
-    const maxOffset =
-      event.nativeEvent.contentSize.height -
-      event.nativeEvent.layoutMeasurement.height;
 
     setShowButtonToTop(currentOffset > 900);
   };
@@ -157,9 +192,9 @@ const Listings: React.FC<ListingsProps> = ({ items, category }) => {
         ref={listRef}
         data={data}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={renderRow}
-        initialNumToRender={5}
-        maxToRenderPerBatch={15}
+        renderItem={RenderRow}
+        initialNumToRender={3}
+        maxToRenderPerBatch={6}
         onEndReached={loadMoreData}
         onEndReachedThreshold={0.5}
         ListFooterComponent={renderFooter}
@@ -184,9 +219,20 @@ const styles = StyleSheet.create({
     marginVertical: 12,
     borderRadius: 18,
   },
-  image: {
+  imageContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
     width: "100%",
     height: 300,
+    borderRadius: 18,
+    overflow: "hidden",
+    backgroundColor: "#EAEAEA",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
     borderRadius: 18,
     overflow: "hidden",
   },
@@ -203,6 +249,7 @@ const styles = StyleSheet.create({
   },
   likeBtn: {
     position: "absolute",
+    color: "white",
     right: 30,
     top: 30,
   },
@@ -231,8 +278,8 @@ const styles = StyleSheet.create({
       width: -15,
       height: -5,
     },
-    shadowOpacity: 1, // This makes the shadow visible
-    shadowRadius: 0, // This makes the shadow sharp, not blurred
+    shadowOpacity: 1,
+    shadowRadius: 0,
   },
   price: {
     flexDirection: "column",
@@ -240,7 +287,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     textAlign: "center",
     backgroundColor: "#FF5A5F",
-    // backgroundColor: "#FF9194",
     padding: 2,
     height: "100%",
     width: "100%",
@@ -262,8 +308,8 @@ const styles = StyleSheet.create({
       width: -10,
       height: 10,
     },
-    shadowOpacity: 1, // This makes the shadow visible
-    shadowRadius: 0, // This makes the shadow sharp, not blurred
+    shadowOpacity: 1,
+    shadowRadius: 0,
   },
   details: {
     flexDirection: "column",
