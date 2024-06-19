2dcd001cbac8a1141ef9854dc7866230d67b6942
ZikaZaki <17160061@su.edu.ye>
2024-06-19 04:54:56 +0300

    add custom renderCluster function

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Marker, PROVIDER_DEFAULT, Region } from "react-native-maps";
+import { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 import MapView from "react-native-map-clustering";
 import { defaultStyles } from "@/constants/Styles";
 import { AirbnbList } from "@/app/interfaces/airbnb_list";
 import { router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 interface Props {
   listingData: AirbnbList[];
@@ -29,7 +31,7 @@ const debouncedRenderMarkers = (
     }
     timerId = setTimeout(() => {
       setDebouncedListingData(data);
-    }, 300);
+    }, 100);
     return () => {
       if (timerId) {
         clearTimeout(timerId);
@@ -57,18 +59,43 @@ const ListingsMap = ({ listingData }: Props) => {
     return cleanup;
   }, [listingData, debouncedRender]);
 
+  const renderCluster = (cluster: any) => {
+    const { id, properties, geometry, onPress } = cluster;
+    const points = properties.point_count;
+
+    return (
+      <Marker
+        key={id}
+        onPress={onPress}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+      >
+        <View style={styles.cluster}>
+          <Text style={{ fontFamily: "mon-sb", fontSize: 14 }}>{points}</Text>
+        </View>
+      </Marker>
+    );
+  };
+
   return (
     <View style={defaultStyles.container}>
       <MapView
+        animationEnabled={false}
         style={StyleSheet.absoluteFill}
         initialRegion={INITIAL_REGION}
         showsUserLocation
         showsMyLocationButton
         provider={PROVIDER_DEFAULT}
+        clusterColor="#fff"
+        clusterTextColor="#000"
+        clusterFontFamily="mon-sb"
+        renderCluster={renderCluster}
       >
         {debouncedListingData.map((item) => (
           <Marker
-            key={item.id + item.host_id}
+            key={`${item.id}${item.host_id}`}
             coordinate={{
               latitude: item?.latitude,
               longitude: item?.longitude,
@@ -76,6 +103,7 @@ const ListingsMap = ({ listingData }: Props) => {
             onPress={() => onMarkerSelected(item)}
           >
             <View style={styles.marker}>
+              <Ionicons name="location" size={20} color={Colors.primary} />
               <Text style={styles.markerText}>
                 {item?.price ? item.price : "$N/A"}
               </Text>
@@ -91,9 +119,11 @@ export default ListingsMap;
 
 const styles = StyleSheet.create({
   marker: {
-    backgroundColor: "#fff",
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    gap: 3,
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 6,
     elevation: 5,
@@ -108,5 +138,20 @@ const styles = StyleSheet.create({
   markerText: {
     fontSize: 14,
     fontFamily: "mon-sb",
+  },
+  cluster: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    padding: 8,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
   },
 });
