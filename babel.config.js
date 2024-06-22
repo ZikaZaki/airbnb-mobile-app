75207525c3c41256a579e8571e9c256723075f35
ZikaZaki <17160061@su.edu.ye>
2024-06-22 20:30:05 +0300

    fix dependency conflicts

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: [
+      "babel-preset-expo", // This already includes support for expo-router
+    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
